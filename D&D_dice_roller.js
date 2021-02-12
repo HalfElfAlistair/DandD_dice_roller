8bede5fb872ce1245c4554a463d673ec922a3b87
HalfElfAlistair <ajss90@hotmail.co.uk>
2021-02-12 18:32:47 +0000

    amended html so it loads js from a separate file rather than within the html

--- D&D_dice_roller.js
+++ D&D_dice_roller.js
@@ -1,13 +1,11 @@
-
-
         function d20Roller() {
-          let result = (Math.floor(Math.random() * 20) + 1);
-          document.getElementById('score').innerHTML = "Score: " + result;
-          if (result === 20) {
-          document.getElementById('crit').innerHTML = "CRIT! CRIT! CRIT!";
-        } else if (result === 1) {
-          document.getElementById('crit').innerHTML = "CRITICAL FAIL!";
-        } else {
-          document.getElementById('crit').innerHTML = "";
-        }
+            let result = (Math.floor(Math.random() * 20) + 1);
+            document.getElementById('20score').innerHTML = "Score: " + result;
+            if (result === 20) {
+            document.getElementById('crit').innerHTML = "CRIT! CRIT! CRIT!";
+          } else if (result === 1) {
+            document.getElementById('crit').innerHTML = "CRITICAL FAIL!";
+          } else {
+            document.getElementById('crit').innerHTML = "";
+          }
         }
